7ecd34a9a47fb69bad0d874d7b3eac5ebf9996e9
CHIRAGGARG1697 <chiraggarg.0106@gmail.com>
2024-09-04 15:33:02 +0530

    First Commit

---
+++ FindingMonthsWeeksDays.py
@@ -0,0 +1,6 @@
+age = input("Enter your current age")
+final_age = 90-int(age)
+total_months = final_age*12
+total_weeks = final_age*52
+total_days = final_age*365
+print(f"Total No of Months are{total_months},Whereas total no of weeks are {total_weeks} and total no of days are{total_days}")
